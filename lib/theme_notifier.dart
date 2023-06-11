b18182027f1dc80814a83b0bd355d4ad549589be
MannavaVivek <vivekmannava@gmail.com>
2023-06-11 12:15:26 +0200

    Saving theme across restarts

--- lib/theme_notifier.dart
+++ lib/theme_notifier.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier with ChangeNotifier {
   ThemeData _currentTheme;
+  final String key = "theme";
 
-  ThemeNotifier(this._currentTheme);
+  ThemeNotifier(this._currentTheme) {
+    _loadTheme();
+  }
+  
+  _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    String theme = prefs.getString(key) ?? 'light';
+    
+    if (theme == 'light') {
+      _currentTheme = ThemeData.light();
+    } else {
+      _currentTheme = ThemeData.dark();
+    }
+    notifyListeners();
+  }
   
   getTheme() => _currentTheme;
 
-  setTheme(ThemeData theme) {
-    _currentTheme = theme;
+  setTheme(ThemeData theme) async {
+    final prefs = await SharedPreferences.getInstance();
+    
+    if (theme == ThemeData.light()) {
+      prefs.setString(key, 'light');
+      _currentTheme = ThemeData.light();
+    } else {
+      prefs.setString(key, 'dark');
+      _currentTheme = ThemeData.dark();
+    }
     notifyListeners();
   }
 }
