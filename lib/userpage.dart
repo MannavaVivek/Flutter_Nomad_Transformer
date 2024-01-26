24ca311cb9b0ee81dec3ba149b7f314b7e8d5538
MannavaVivek <vivekmannava@gmail.com>
2024-01-26 20:47:38 +0100

    Revamp user page

--- lib/userpage.dart
+++ lib/userpage.dart
@@ -5,13 +5,13 @@ import 'package:isar/isar.dart';
 class UserPage extends StatefulWidget {
   final Isar isar;
 
-  UserPage({Key? key, required this.isar}) : super(key: key);
+  const UserPage({Key? key, required this.isar}) : super(key: key);
 
   @override
-  _UserPageState createState() => _UserPageState();
+  UserPageState createState() => UserPageState();
 }
 
-class _UserPageState extends State<UserPage> {
+class UserPageState extends State<UserPage> {
   final FirebaseAuth auth = FirebaseAuth.instance;
   String _email = '';
   String _password = '';
@@ -21,47 +21,123 @@ class _UserPageState extends State<UserPage> {
     User? user = auth.currentUser;
 
     return Scaffold(
-      appBar: AppBar(title: const Text("User Page")),
-      body: Center(
+      body: user == null
+          ? _buildGuestView(context)
+          : _buildUserView(context, MediaQuery.of(context).size),
+    );
+  }
+
+  Widget _buildUserView(BuildContext context, Size constraints) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: <Widget>[
+          const SizedBox(height: 100),
+          const CircleAvatar(
+            radius: 70,
+            backgroundImage: AssetImage('assets/programmer.png'),
+            backgroundColor: Colors.transparent,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            "You are logged in as \n${auth.currentUser!.email}",
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          _buildActionContainer(context),
+          const SizedBox(height: 100),
+          const Text('Ver. 0.0.7'),
+          _buildSignOutButton(context),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionContainer(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 30, left: 20, right: 20),
+        width: double.infinity,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Image in the center within a circle
-            const CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage(
-                  'assets/user_image.png'), // Replace with your asset image
-              backgroundColor: Colors.transparent,
-            ),
-            const SizedBox(height: 16),
-            // Text indicating user status
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  user == null
-                      ? 'Currently using as Guest'
-                      : 'Currently logged in as',
-                  textAlign: TextAlign.center,
-                ),
-                if (user != null)
-                  Text(
-                    user.email ?? '', // Safely handle null email
-                    textAlign: TextAlign.center,
-                  ),
-              ],
-            ),
-
-            const SizedBox(height: 16),
-            // Login or Sign out button
-            user == null
-                ? _buildLoginButton(context)
-                : _buildSignOutButton(context),
-
-            user == null ? _buildSignUpLink(context) : Container(),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextButton(
+                onPressed: () => _showDialog(context, 'Help',
+                    'You click on blog posts to read them. If you like em, click on the heart icon to favorite them. You can view your favorites by clicking on the heart icon on the bottom navigation bar. You can alo search for blog posts by clicking on the search icon on the bottom navigation bar.'),
+                child: const Text('Help')),
+            _customDivider(),
+            TextButton(
+                onPressed: () => _showDialog(context, 'Report',
+                    'If you have this app, you probably know me, so just let me know.'),
+                child: const Text('Report')),
+            _customDivider(),
+            TextButton(
+                onPressed: () => _showDialog(context, 'About',
+                    'For now, this is a project for fun, and for me to learn Flutter. I hope you enjoy it!'),
+                child: const Text('About')),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _customDivider() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      height: 1,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.transparent,
+            Color.fromARGB(163, 221, 220, 220),
+            Colors.transparent
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+      ),
+    );
+  }
+
+  void _showDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildGuestView(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const CircleAvatar(
+            radius: 75,
+            backgroundImage: AssetImage('assets/user_image.png'),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'Currently using as Guest',
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30),
+          _buildLoginButton(context),
+          _buildSignUpLink(context),
+        ],
       ),
     );
   }
@@ -169,10 +245,10 @@ class _UserPageState extends State<UserPage> {
   }
 
   Future<bool> _showSignUpDialog(BuildContext context) async {
-    String _email = '';
-    String _password = '';
-    String _confirmPassword = '';
-    String _errorMessage = '';
+    String email = '';
+    String password = '';
+    String confirmPassword = '';
+    String errorMessage = '';
 
     bool signUpSuccessful = await showDialog(
       context: context,
@@ -185,47 +261,47 @@ class _UserPageState extends State<UserPage> {
             content: SingleChildScrollView(
               child: ListBody(
                 children: <Widget>[
-                  _errorMessage.isNotEmpty
-                      ? Text(_errorMessage,
+                  errorMessage.isNotEmpty
+                      ? Text(errorMessage,
                           style: const TextStyle(color: Colors.red))
                       : Container(),
                   TextField(
                     decoration: InputDecoration(
                       labelText: 'Email',
-                      errorText: _email.isNotEmpty &&
+                      errorText: email.isNotEmpty &&
                               !RegExp(r"^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                  .hasMatch(_email)
+                                  .hasMatch(email)
                           ? 'Invalid email format'
                           : null,
                     ),
                     keyboardType: TextInputType.emailAddress,
                     onChanged: (value) {
-                      _email = value;
+                      email = value;
                     },
                   ),
                   TextField(
                     decoration: InputDecoration(
                       labelText: 'Password',
-                      errorText: _password.isNotEmpty && _password.length < 8
+                      errorText: password.isNotEmpty && password.length < 8
                           ? 'Password must be at least 8 characters'
                           : null,
                     ),
                     obscureText: true,
                     onChanged: (value) {
-                      _password = value;
+                      password = value;
                     },
                   ),
                   TextField(
                     decoration: InputDecoration(
                       labelText: 'Confirm Password',
-                      errorText: _confirmPassword.isNotEmpty &&
-                              _password != _confirmPassword
+                      errorText: confirmPassword.isNotEmpty &&
+                              password != confirmPassword
                           ? 'Passwords do not match'
                           : null,
                     ),
                     obscureText: true,
                     onChanged: (value) {
-                      _confirmPassword = value;
+                      confirmPassword = value;
                     },
                   ),
                 ],
@@ -241,30 +317,30 @@ class _UserPageState extends State<UserPage> {
               TextButton(
                 child: const Text('Sign Up'),
                 onPressed: () async {
-                  if (_email.isEmpty ||
-                      _password.isEmpty ||
-                      _confirmPassword.isEmpty ||
-                      _password.length < 8 ||
-                      _password != _confirmPassword ||
+                  if (email.isEmpty ||
+                      password.isEmpty ||
+                      confirmPassword.isEmpty ||
+                      password.length < 8 ||
+                      password != confirmPassword ||
                       !RegExp(r"^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                          .hasMatch(_email)) {
-                    _errorMessage = 'Please fill in all fields correctly';
+                          .hasMatch(email)) {
+                    errorMessage = 'Please fill in all fields correctly';
                   } else {
                     try {
                       await FirebaseAuth.instance
                           .createUserWithEmailAndPassword(
-                              email: _email, password: _password);
+                              email: email, password: password);
 
                       Navigator.of(context).pop(true);
                     } catch (e) {
                       if (e is FirebaseAuthException) {
                         if (e.code == 'email-already-in-use') {
-                          _errorMessage = 'User email already exists';
+                          errorMessage = 'User email already exists';
                         } else {
-                          _errorMessage = 'Failed to create user: ${e.message}';
+                          errorMessage = 'Failed to create user: ${e.message}';
                         }
                       } else {
-                        _errorMessage = 'An unknown error occurred';
+                        errorMessage = 'An unknown error occurred';
                       }
                       setState(() {}); // Update UI
                     }
@@ -276,21 +352,6 @@ class _UserPageState extends State<UserPage> {
         });
       },
     );
-
     return signUpSuccessful;
-  }
-
-  Future<void> _createUser(
-      String email, String password, BuildContext context) async {
-    try {
-      await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
-      Navigator.of(context).pop(); // Close the dialog
-      setState(() {}); // Update UI
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to create user: $e')),
-      );
-    }
   }
 }
